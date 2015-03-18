525d0c9cf4ed906c8d9792c0619e5acb10f66e0f
Delaine Lawrence <delaine.lawrence35@gmail.com>
2015-03-18 11:16:44 -0400

    Basics

---
+++ basics/exercise_3.rb
@@ -0,0 +1,11 @@
+movies = { The_Walking_Dead: 2015,
+		   Taken_3: 2014,
+		   Seventh_Son: 2013,
+		   Blackhat: 2012,
+		   Top_Five: 2011 }
+
+puts movies[:The_Walking_Dead]
+puts movies[:Taken_3]
+puts movies[:Seventh_Son]
+puts movies[:Blackhat]
+puts movies[:Top_Five]
---
+++ basics/exercise_4.rb
@@ -0,0 +1,8 @@
+dates = [2011, 2012, 2013, 2014, 2015]
+
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
