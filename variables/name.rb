b96700cca6f06b472a7587206cbb2ad91a681189
Delaine Lawrence <delaine.lawrence35@gmail.com>
2015-03-18 11:40:42 -0400

    Variables

--- variables/name.rb
+++ variables/name.rb
@@ -1,7 +1,14 @@
 # name.rb
 
 puts "What is your name?"
-
 name = gets.chomp
-
 puts "Hello " + name
+10.times do
+	puts name
+end
+
+puts "First name please!"
+fname = gets.chomp
+puts "Last name please"
+lname = gets.chomp
+puts "You shall be called #{fname} #{lname}."
